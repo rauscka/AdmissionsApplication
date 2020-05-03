21d89bc8a22ca6b697fd1fe4e64e8c9186639e76
beccadura <52724406+beccadura@users.noreply.github.com>
2020-05-02 21:56:55 -0500

    database queries now work & data is displayed on About Screen

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,18 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  //parserOptions: {
+    //ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
+    //sourceType: 'module', // Allows for the use of imports
+  //},
+  //"parser": "babel-eslint",
+  //"parserOptions": {
+    //"ecmaFeatures": {
+      //"jsx": true,
+      //"modules": true
+    //}
+  //}
 };
+
+
 
--- screens/Aboutscreen.js
+++ screens/Aboutscreen.js
@@ -1,28 +1,83 @@
-import React from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {StyleSheet, Text, View, Button, FlatList, ActivityIndicator} from 'react-native';
+//import {firebase, firestore} from '@react-native-firebase/firestore';
 import firestore from '@react-native-firebase/firestore';
+
+
+function Users() {
+    const [loading, setLoading] = useState(true); // Set loading to true on component mount
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        const subscriber = firestore()
+            .collection("tourGuides")
+            .onSnapshot(querySnapshot => {
+                const users = [];
+
+                querySnapshot.forEach(documentSnapshot => {
+                    //console.log('User ID: ', documentSnapshot.id, documentSnapshot.data());
+                    users.push({
+                        ...documentSnapshot.data(),
+                        key: documentSnapshot.id,
+                    });
+                });
+
+
+                setUsers(users);
+                setLoading(false);
+            });
+
+        // Unsubscribe from events when no longer in use
+        return () => subscriber();
+    }, []);
+    if (loading) {
+        return <ActivityIndicator />;
+    }
+
+    return (
+        <FlatList
+            data={users}
+            renderItem={({ item }) => (
+                <View style={{ height: 80, flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                    <Text>User ID: {item.student_id}</Text>
+                    <Text>User Name: {item.first_name}</Text>
+                    <Text>User Name: {item.last_name}</Text>
+                </View>
+            )}
+        />
+    );
+}
+
 
 
 export default class Aboutscreen extends React.Component {
   render() {
     const {navigate} = this.props.navigation;
+
     return (
       <View style={styles.container}>
         <Text>About Drake</Text>
-          <Text>
-              Trying to insert Firebase data here
-          </Text>
-        <Button title={'Home Page'} onPress={() => navigate('Home')} />
+          <Button title={'Home Page'} onPress={() => navigate('Home')} />
+      <View style={styles.containerTwo}>
+          {<Users />}
       </View>
+      </View>
+
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#25ffe2',
-  },
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#25ffe2',
+    },
+    containerTwo: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: '#25ffe2',
+    },
 });
