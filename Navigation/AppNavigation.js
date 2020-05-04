92e511fcb2d8e9e557b0c4f45dadb5fb43e12856
rauscka <40070150+rauscka@users.noreply.github.com>
2020-05-04 12:36:32 -0500

    Added icons and messaging tabs. Prior to adding new library UI kitten

--- Navigation/AppNavigation.js
+++ Navigation/AppNavigation.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import {Header} from "react-native-elements";
 import HomeScreen from "../screens/Homescreen";
 import AboutScreen from "../screens/Aboutscreen";
 import {Button, Image, StyleSheet} from "react-native";
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import Message from '../screens/Message';
 
 
 function LogoTitle() {
@@ -32,9 +35,30 @@ class AppNavigation extends React.Component {
                     centerComponent={{text: 'Drake Admissions App', style: {color: '#fff'}}}
                     rightComponent={<Button title={'Log Out'} onPress={() => navigate('Auth')}/>}
                 />
-                <Tab.Navigator>
+                <Tab.Navigator
+                    screenOptions={({route}) => ({
+                        tabBarIcon: ({focused, color, size}) => {
+                            let iconName;
+
+                            if (route.name === 'Home') {
+                                iconName = focused ? 'md-paw' : 'md-paw';
+                            } else if (route.name === 'About Drake') {
+                                iconName = focused ? 'ios-list-box' : 'ios-list';
+                            } else if (route.name === 'Message') {
+                                iconName = focused ? 'ios-chatbubbles' : 'ios-chatbubbles';
+                            }
+
+                            // You can return any component that you like here!
+                            return <Ionicons name={iconName} size={size} color={color} />;
+                        },
+                    })}
+                    tabBarOptions={{
+                        activeTintColor: '#3b43ff',
+                        inactiveTintColor: 'gray',
+                    }}>
                     <Tab.Screen name="Home" component={HomeScreen}/>
                     <Tab.Screen name="About Drake" component={AboutScreen}/>
+                    <Tab.Screen name ="Message" component={Message}/>
                 </Tab.Navigator>
             </NavigationContainer>
         );
